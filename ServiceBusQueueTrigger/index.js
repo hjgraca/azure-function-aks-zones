587443152675e8c2bbef0235c1ef1d974838dd5b
Henrique Graca <henrique.j.graca@gmail.com>
2021-03-30 15:40:35 +0100

    first commit

---
+++ ServiceBusQueueTrigger/index.js
@@ -0,0 +1,7 @@
+module.exports = function (context, myQueueItem) {
+    context.log('Node.js ServiceBus queue trigger function processed message', myQueueItem);
+    context.log('EnqueuedTimeUtc =', context.bindingData.enqueuedTimeUtc);
+    context.log('DeliveryCount =', context.bindingData.deliveryCount);
+    context.log('MessageId =', context.bindingData.messageId);
+    context.done();
+};
